ab0e95fe6135571228d7646be87613992fdd34bf
tyboe2013 <40647812+tyboe2013@users.noreply.github.com>
2022-07-30 22:38:44 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,13 +3,14 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import math
 import cv2
 import numpy as np
 import platform
 import time
 import string
 import random
-
+import pytesseract
 
 count = 0
 #ID's of items
@@ -31,32 +32,41 @@ options.add_argument("--disable-gpu")  # applicable to windows os only
 # overcome limited resource problems
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--no-sandbox")  # Bypass OS security model
+coordinates = []
+def shape_selection(event, x, y, flags, param):
+    global coordinates
+    if event == cv2.EVENT_LBUTTONDOWN:
+        coordinates = [(x, y)]
+    elif event == cv2.EVENT_LBUTTONUP:
+        coordinates.append((x,y))
+        cv2.rectangle(img, coordinates[0],coordinates[2], (0,0,255),2)
+        cv2.imshow('image', img)
 
 img = cv2.imread('0-ttc.png')
+cv2.namedWindow('img')
+cv2.setMouseCallback('img', shape_selection)
 rows, cols, _ = img.shape
-print('Rows', rows)
-print('Cols', cols)
 
 cut_image = img[635: 775, 35: 950]
-
-cv2.imshow("Cut image", cut_image)
+cv2.imwrite('ttc.png', cut_image)
 cv2.waitKey(0)
+crop = cv2.imread('ttc.png')
+crop_copy = crop.copy()
+config = ('-l eng --oem 1 --psm 3')
+pytesseract.pytesseract.tesseract_cmd = 'C://Program Files//Tesseract-OCR//tesseract.exe'
+text = pytesseract.image_to_string(crop, config=config)
+
+while (True):
+    cv2.imshow('crop', crop)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key==13:
+        break
+    if key == ord('c'):
+        crop = crop_copy.copy()
 
 
-with webdriver.Chrome(path, chrome_options=options) as driver:
-        # these values represent the sizes of the entire browser window and not the viewport.
-        for link in links:
-            tablet = {'output': str(count) + '-ttc.png',
-                    'width': 1000,
-                    'height': 800}
-            post = str(count) + '-ttc.png'
-            images.append(post)
-            #Variable adds increments too change name of each screenshots too prevent duplicates
-            count += 1
-            #Main url
-            linkWithProtocol = 'https://us.tamrieltradecentre.com/pc/Trade/SearchResult?ItemID=' + str(link) + '&SortBy=Price&Order=asc'
-            #Sets browser size, gets Website, Takes Screenshot of the site
-            driver.set_window_size(tablet['width'], tablet['height'])
-            driver.get(linkWithProtocol)
-            WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='search-result-view']/div[1]/div/table/tbody/tr[1]/td[4]")))
-            driver.save_screenshot(tablet['output'])
+if len(coordinates) == 2:
+    image_roi = crop_copy[coordinates[0][1]:coordinates[1][1],
+                                coordinates[0][0]:coordinates[1][0]]
+    cv2.show('roi press any key', image_roi)
